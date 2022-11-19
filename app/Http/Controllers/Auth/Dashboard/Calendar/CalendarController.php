ff2413934e681585db78110b08e7fa038491f9d3
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2022-11-19 20:06:00 +0100

    refactored: - remove monthlyCalendar from controller to blade
    add:
    - show weekend (Sut/Sun)

--- app/Http/Controllers/Auth/Dashboard/Calendar/CalendarController.php
+++ app/Http/Controllers/Auth/Dashboard/Calendar/CalendarController.php
@@ -56,100 +56,28 @@ class CalendarController extends DashboardController
         return $days_in_month;
     }
 
-    private function createMonthlyCalendar()
+    private function getMonthWeeks()
     {
-        //creat weeks
         $weeks = [];
-        foreach ($this->days_of_month as $day) {
-            $weeks[$day->weekNumberInMonth][] = $day;
+        foreach ($this->days_of_month as $key => $day) {
+            $weeks[$day->weekNumberInMonth][] = [
+                'day' =>$day,
+                'appointments' => $this->createAppointments($day)
+            ] ;
         }
-
-        $first_week = array_map(function ($day) {
-            return $day->dayOfWeekIso;
-        }, $weeks[1]);
-
-
-        $html = '<div class="calendar"> ';
-
-        //create name days of week
-        $days = '<div class="days">';
-        $period = CarbonPeriod::create('2022-11-07', '2022-11-13');
-        foreach ($period->toArray() as $day) {
-            $days .= "<p class='day'>{$day->locale(config('global.user.location'))->shortDayName} </p>";
-        }
-        $days .= '</div>';
-
-        $html .= $days;
-
-
-        for ($week = 1; $week <= count($weeks); $week++) {
-            $html .= '<div class="week"> ';
-
-            $first_week_html = '';
-            $other_week_html = '';
-            // create first week
-            if ($week === 1) {
-                for ($day = 1; $day <= 7; $day++) {
-                    $day_index = array_search($day, $first_week);
-                    if ($day_index || $day_index === 0) {
-                        if(($weeks[$week][$day_index]->day === $this->today->day) &&
-                            ($this->chose_month->month === $this->today->month) &&
-                            ($this->chose_month->year === $this->today->year)
-                        ) {
-                            $first_week_html .= "<a class='day today' href='/calendar/day?day={$weeks[$week][$day_index]->format("Y-m-d")}'><p class='date'>{$weeks[$week][$day_index]->day}</p><div class='appointments'>{$this->createAppointments($weeks[$week][$day_index]->toDateString())}</div></a>";
-                        } else {
-                            $first_week_html .= "<a class='day' href='/calendar/day?day={$weeks[$week][$day_index]->format("Y-m-d")}'><p class='date'>{$weeks[$week][$day_index]->day}</p><div class='appointments'>{$this->createAppointments($weeks[$week][$day_index]->toDateString())}</div></a>";
-                        }
-                    } else {
-                        $first_week_html .= "<p class='day _other'></p>";
-                    }
-                }
-                $html .= $first_week_html;
-            } else {
-                for ($day = 0; $day < 7; $day++) {
-                    if (isset($weeks[$week][$day])) {
-                        if (($weeks[$week][$day]->day === $this->today->day) &&
-                            ($this->chose_month->month === $this->today->month)&&
-                            ($this->chose_month->year === $this->today->year)
-                        ) {
-                            $other_week_html .= "<a class='day today' href='/calendar/day?day={$weeks[$week][$day]->format("Y-m-d")}'><p class='date'>{$weeks[$week][$day]->day}</p><div class='appointments'>{$this->createAppointments($weeks[$week][$day]->toDateString())}</div></a>";
-                        } else {
-                            $other_week_html .= "<a class='day' href='/calendar/day?day={$weeks[$week][$day]->format("Y-m-d")}'><p class='date'>{$weeks[$week][$day]->day}</p><div class='appointments'>{$this->createAppointments($weeks[$week][$day]->toDateString())}</div></a>";
-                        }
-                    } else {
-                        $other_week_html .= "<p class='day _other'></p>";
-                    }
-                }
-                $html .= $other_week_html;
-            }
-            $html .= '</div>';
-        }
-        $html .= '</div>';
-        return $html;
-//        return $weeks;
+        return $weeks;
     }
 
-    private function createAppointments($day){
 
-        $day_appointments = array_filter($this->appointments,function($element) use ($day) {
+    private function createAppointments($booked_day): array
+    {
+        return array_filter($this->appointments,function($element) use ($booked_day) {
             $date =  Carbon::parse($element['date']);
-            if($date->toDateString() === $day ){
-                return $day;
+            if($date->toDateString() === $booked_day->toDateString() ){
+                return $booked_day;
             }
+
         });
-
-
-        $count = count($day_appointments);
-
-        if ($count>0){
-            return "<i class='icon icon_client'></i><p>({$count})</p>";
-        }
-
-//        $html = '';
-//        foreach ($day_appointments as $appointment){
-//            $html.= "<p class='appointment'></p>";
-//        }
-//        return $html;
     }
 
     public function index(Request $request)
@@ -165,8 +93,8 @@ class CalendarController extends DashboardController
             'choseMonth' => $this->chose_month,
             'nextMonth' => $this->getNextMonth(),
             'today' => $this->today->timezone($userTimezone),
-            'calendar' => $this->createMonthlyCalendar(),
-            'appointments'=> $this->appointments
+            'appointments'=> $this->appointments,
+            'weeks'=> $this->getMonthWeeks(),
         ]);
     }
 }
--- resources/views/auth/dashboard/calendar/calendar.blade.php
+++ resources/views/auth/dashboard/calendar/calendar.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.dashboard')
+@dump($weeks)
 
 @section('dashboard.content')
     <div class="page-calendar">
@@ -20,7 +21,74 @@
                     </a>
                 </div>
             </div>
-             {!! $calendar !!}
+{{--             {!! $calendar !!}--}}
+
+
+            @php
+                $first_week = array_map(function ($day) {
+                return $day['day']->dayOfWeekIso;
+                }, $weeks[1]);
+            @endphp
+
+            <div class="calendar">
+                <div class="days">
+                    <p class="day">{{__("Mon")}}</p>
+                    <p class="day">{{__("Tue")}}</p>
+                    <p class="day">{{__("Wed")}}</p>
+                    <p class="day">{{__("Thu")}}</p>
+                    <p class="day">{{__("Fri")}}</p>
+                    <p class="day">{{__("Sat")}}</p>
+                    <p class="day">{{__("Sun")}}</p>
+                </div>
+            @foreach($weeks as  $week )
+                @if($loop->first)
+                    <div class="week">
+                        @for($day = 1; $day <= 7; $day++)
+                            @php($day_index = array_search($day, $first_week))
+                            @if ($day_index || $day_index === 0)
+                                <a class='day {{($today->day === $week[$day_index]['day']->day) && ($today->month === $choseMonth->month) && ($today->year === $choseMonth->year)? 'today' :''}}
+                                {{$day == 6 || $day==7 ? 'weekend' :''}}'
+                                    href='/calendar/day?day={{$week[$day_index]['day']->format("Y-m-d")}}'
+                                >
+                                    <span class='date'>{{$week[$day_index]['day']->day}}</span>
+                                    <div class='appointments'>
+                                        @if(count($week[$day_index]['appointments']) >0)
+                                            <i class='icon icon_client'></i>
+                                            <p>({{count($week[$day_index]['appointments'])}})</p>
+                                        @endif
+                                    </div>
+                                </a>
+                            @else
+                                <p class='day _anotherMonth {{$day === 6 || $day === 7 ? 'weekend' :''}}'></p>
+                            @endif
+                        @endfor
+                    </div>
+                @else
+                    <div class="week">
+                        @for($day = 1; $day <= 7; $day++)
+                            @if (isset($week[$day-1]))
+
+                            <a class='day {{($today->day === $week[$day-1]['day']->day) && ($today->month === $choseMonth->month) && ($today->year === $choseMonth->year)? 'today' :''}}
+                            {{$day == 6 || $day==7 ? 'weekend' :''}}'
+                               href='/calendar/day?day={{$week[$day-1]['day']->format("Y-m-d")}}'
+                            >
+                                <span class='date'>{{$week[$day-1]['day']->day}}</span>
+                                <div class='appointments'>
+                                    @if(count($week[$day-1]['appointments']) >0)
+                                        <i class='icon icon_client'></i>
+                                        <p>({{count($week[$day-1]['appointments'])}})</p>
+                                    @endif
+                                </div>
+                            </a>
+                            @else
+                                <p class='day _anotherMonth {{$day === 6 || $day === 7 ? 'weekend' :''}}'></p>
+                            @endif
+                        @endfor
+                    </div>
+                @endif
+
+                @endforeach
+            </div>
         </div>
 
 
