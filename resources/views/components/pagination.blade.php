2b71ec57f25f112800d445617a8cbd780238d297
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2022-12-24 16:46:30 +0100

    refactored : - exchange pagination - clean code in web.php

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -1,10 +1,10 @@
-
-
-<div class="pagination">
-    <div class="pagination__results">
-       <p>{{__('Results')}}: {{$paginator->firstItem()}} - {{$paginator->lastItem()}} {{__('of')}} {{$paginator->total()}}</p>
-    </div>
-    <div class="pagination_navigation">
+@if($paginator->hasPages())
+    <div class="pagination">
+        <div class="pagination__results">
+            <p>{{__('Results')}}: {{$paginator->firstItem()}}
+                - {{$paginator->lastItem()}} {{__('of')}} {{$paginator->total()}}</p>
+        </div>
+        <div class="pagination_navigation">
 
             @if($paginator->onFirstPage())
                 <a class="btn last_page inactive" href="{{$paginator->previousPageUrl()}}">Prev</a>
@@ -16,22 +16,22 @@
             <div class="pagination__links">
                 @for ($count = 1; $count <= $paginator->lastPage(); $count++)
 
-                @if ($paginator->currentPage() > 4 && $count === 2)
-                    <span class="btn dots">...</span>
-                @endif
+                    @if ($paginator->currentPage() > 4 && $count === 2)
+                        <span class="btn dots">...</span>
+                    @endif
 
 
-                @if ($count === $paginator->currentPage())
-                    <span class="btn active">{{ $count }}</span>
-                @elseif ($count === $paginator->currentPage() + 1 || $count === $paginator->currentPage() + 2 || $count === $paginator->currentPage() - 1 || $count === $paginator->currentPage() - 2 || $count === $paginator->lastPage() || $count === 1)
-                    <a class="btn" href="{{ $paginator->url($count) }}">{{ $count }}</a>
-                @endif
+                    @if ($count === $paginator->currentPage())
+                        <span class="btn active">{{ $count }}</span>
+                    @elseif ($count === $paginator->currentPage() + 1 || $count === $paginator->currentPage() + 2 || $count === $paginator->currentPage() - 1 || $count === $paginator->currentPage() - 2 || $count === $paginator->lastPage() || $count === 1)
+                        <a class="btn" href="{{ $paginator->url($count) }}">{{ $count }}</a>
+                    @endif
 
 
-                @if ($paginator->currentPage() < $paginator->lastPage() - 3 && $count === $paginator->lastPage() - 1)
-                    <span class="btn dots">...</span>
-                @endif
-            @endfor
+                    @if ($paginator->currentPage() < $paginator->lastPage() - 3 && $count === $paginator->lastPage() - 1)
+                        <span class="btn dots">...</span>
+                    @endif
+                @endfor
             </div>
 
             @if($paginator->currentPage() === $paginator->lastPage())
@@ -40,4 +40,6 @@
                 <a class="btn last_page" href="{{$paginator->nextPageUrl()}}">Next</a>
             @endif
         </div>
-</div>
+    </div>
+@endif
+
--- routes/web.php
+++ routes/web.php
@@ -14,29 +14,20 @@ use App\Http\Controllers\Auth\User\LoginController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ServiceController;
-use App\View\Components\CreateDailyAppointmentController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'] )->name('main');
 
 Route::name('company.')->group(function (){
-
     Route::get('/register-1', [CreateController::class, 'step1'] )->name('step1');
     Route::post('/register-1', [CreateController::class, 'createOwner'] )->name('createOwner');
-
     Route::get('/register-2', [CreateController::class, 'step2'] )->name('step2');
     Route::post('/register-2', [CreateController::class, 'createCompany'] )->name('createCompany');
-
     Route::get('/register-3', [CreateController::class, 'step3'] )->name('step3');
     Route::post('/register-3', [CreateController::class, 'addPhotoCompany'] )->name('addPhotoCompany');
-
     Route::get('/register-4', [CreateController::class, 'step4'] )->name('step4');
-
     Route::get('/register-5', [CreateController::class, 'step5'] )->name('step5');
-
     Route::get('/register-6', [CreateController::class, 'step6'] )->name('step6');
-
     Route::get('/register-7', [CreateController::class, 'step7'] )->name('step7');
 });
 Route::name('services.')->group(function (){
@@ -51,46 +42,37 @@ Route::name('employee.')->group(function (){
     Route::put('/employee/{id}', [EmployeeController::class, 'update'] )->name('update');
     Route::delete('/employee/{id}', [EmployeeController::class, 'destroy'] )->name('destroy');
 });
-
-
 Route::name('scheduled.')->group(function (){
+    // scheduled company
     Route::get('/scheduled/{id}/{table}', [HomeScheduledController::class, 'show'] )->name('index')->where(['id'=>'[0-9]+','table'=>'^((?!edit$).)*$']);
-});
-Route::name('scheduled.')->group(function (){
     Route::post('/company-scheduled/{id}/create', [CreateCompanyScheduled::class, 'store'] )->name('company.store')->where('id','[0-9]+');
     Route::get('/company-scheduled/{id}/edit', [EditCompanyScheduled::class, 'index'] )->name('company.edit')->where('id','[0-9]+');
     Route::put('/company-scheduled/update', [EditCompanyScheduled::class, 'update'] )->name('company.update');
-});
-Route::name('scheduled.')->group(function (){
+    //scheduled employee
     Route::post('/employee-scheduled/{id}/create', [CreateEmployeeScheduled::class, 'store'] )->name('employee.store')->where('id','[0-9]+');
     Route::get('/employee-scheduled/{id}/edit', [EditEmployeeScheduled::class, 'index'] )->name('employee.edit')->where('id','[0-9]+');
     Route::put('/employee-scheduled/update', [EditEmployeeScheduled::class, 'update'] )->name('employee.update');
 });
-
-
 Route::name('user.')->group(function (){
     Route::get('/login', [LoginController::class, 'index'] )->name('login');
     Route::post('/login', [LoginController::class, 'login'] );
-
-
     Route::get('/logout', [LoginController::class,'logout'] )->name('logout');
 });
-
 Route::name('dashboard.')->middleware('auth')->group(function (){
     Route::get('/main', [DashboardController::class, 'index'] )->name('main');
+
+    //calendar
     Route::get('/calendar/month/{date?}', [CalendarController::class, 'index'] )->name('calendar');
     Route::get('/calendar/day/{date?}', [DailyCalendarController::class, 'index'] )->name('daily_calendar');
 
+    //employee
     Route::get('/employees', [EmployeeController::class, 'show'] )->name('employees');
+    Route::post('/employees/available', [EmployeeController::class, 'getAvailableEmployee'] );
 
-
-
-
-
+    //appointment
     Route::get('/appointment/create/{date}', [AppointmentController::class, 'index'] )->name('index');
     Route::post('/appointment/create', [AppointmentController::class, 'store'] )->name('store');
 
 
-    Route::post('/employees/available', [EmployeeController::class, 'getAvailableEmployee'] );
 });
 
