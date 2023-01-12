5e8a9cbd85ae10334ffc239320398cbac6e5c9ab
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2023-01-12 22:16:36 +0100

    added : - to show information about appointments on dashboard

--- app/Http/Controllers/Auth/DashboardController.php
+++ app/Http/Controllers/Auth/DashboardController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Appointment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
 
     public function index( Request $request)
     {
-        return view('auth.dashboard.main');
+        $appointments_all = Auth::user()->company->appointments;
+        $appointment_model = new Appointment();
+        $appointments_today = $appointment_model->getDailyAppointments(now());
+
+        return view('auth.dashboard.main', [
+            'appointments_all' => count($appointments_all),
+            'appointments_today'=>count($appointments_today),
+        ]);
     }
 }
--- resources/views/auth/dashboard/main.blade.php
+++ resources/views/auth/dashboard/main.blade.php
@@ -3,6 +3,18 @@
 
 @section('dashboard.content')
     <h1>WELCOME TO Dashboard _permanent</h1>
+
+    <div>
+        <span>{{$appointments_all}}</span>
+        <span>{{__('Appointments')}}</span>
+    </div>
+    <div>
+        <span>{{$appointments_today}}</span>
+        <span>{{__('Today Appointments')}}</span>
+    </div>
+
+
+
 @endsection
 
 
