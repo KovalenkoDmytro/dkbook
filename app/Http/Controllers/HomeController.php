f0b57e769974607023dc3f655584ff41e31f9a4a
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2023-03-11 21:58:55 +0100

    add: - styles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class HomeController extends Controller
@@ -12,8 +13,13 @@ class HomeController extends Controller
         $this->middleware('admin');
     }
 
-    public function index() : View
+    public function index() : \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
     {
-        return view('home');
+        if(Auth::check()){
+            return redirect()->route('dashboard.main');
+        }else{
+            return redirect()->route('user.login');
+        }
+//        return view('home');
     }
 }
--- resources/views/auth/dashboard/main.blade.php
+++ resources/views/auth/dashboard/main.blade.php
@@ -2,19 +2,20 @@
 @extends('layouts.dashboard')
 
 @section('dashboard.content')
-    <h1>WELCOME TO Dashboard _permanent</h1>
+    <h1>{{__("WELCOME TO Dashboard")}}</h1>
 
-    <div>
-        <span>{{count($appointments_all)}}</span>
-        <span>{{__('Appointments')}}</span>
+    <div class="statist_items">
+        <div class="statist_item">
+            <i class="fi fi-rr-calendar"></i>
+            <span class="number">{{count($appointments_all)}}</span>
+            <span>{{__('Appointments')}}</span>
+        </div>
+        <div class="statist_item">
+            <i class="fi fi-rr-chart-histogram"></i>
+            <span class="number">{{count($appointments_today)}}</span>
+            <span>{{__('Today Appointments')}}</span>
+        </div>
     </div>
-    <div>
-        <span>{{count($appointments_today)}}</span>
-        <span>{{__('Today Appointments')}}</span>
-    </div>
-
-
-
 @endsection
 
 
